af0452f3e83a50b3152b0f1c85fe4dc0e707bc62
front-hakim <the9616@naver.com>
2024-09-20 00:10:25 +0900

    day 07

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,42 @@
 import SearchLayout from '@/components/search-layout';
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import MovieItem from '@/components/movieItem';
 import style from '@/styles/home.module.css';
 import fetchMovies from '@/lib/fetch-movies';
-import { InferGetServerSidePropsType } from 'next';
+import { InferGetStaticPropsType } from 'next';
 import { MovieData } from '@/types';
 import fetchRandomMovies from '@/lib/fetch-reco-movies';
 
-export const getServerSideProps = async () => {
-  const [allMovies, recoMovies] = await Promise.all([
-    fetchMovies(),
-    fetchRandomMovies(),
-  ]);
+export const getStaticProps = async () => {
+  const allMovies = await fetchMovies();
 
   return {
-    props: { allMovies, recoMovies },
+    props: { allMovies },
   };
 };
+
+// 추천 도서는 실시간으로 변경되어야 한다고 생각되어 클라이언트 사이드에서 데이터 별도 호출
+// 전체 도서는 변경될 일이 없다고 생각되어 getStaticProps로 호출
 const Home = ({
   allMovies,
-  recoMovies,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const [recoMovies, setRecoMovies] = useState<MovieData[]>([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const data = await fetchRandomMovies();
+      setRecoMovies(data);
+    };
+    fetch();
+  }, []);
+
   return (
     <div>
       <section>
         <h2>지금 가장 추천하는 영화</h2>
         <div className={style.recommend}>
           {recoMovies.map((item: MovieData) => (
-            <MovieItem key={item.id} {...item} />
+            <MovieItem key={`reco-${item.id}`} {...item} />
           ))}
         </div>
       </section>
@@ -35,7 +44,7 @@ const Home = ({
         <h2>등록된 모든 영화</h2>
         <div className={style.all}>
           {allMovies.map((item: MovieData) => (
-            <MovieItem key={item.id} {...item} />
+            <MovieItem key={`all-${item.id}`} {...item} />
           ))}
         </div>
       </section>
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,26 +1,31 @@
 import SearchLayout from '@/components/search-layout';
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import MovieItem from '@/components/movieItem';
 import style from '@/styles/search-result.module.css';
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 import fetchSearchMovies from '@/lib/fetch-search-movies';
 import { MovieData } from '@/types';
+import { useRouter } from 'next/router';
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  const q = context.query.q;
+// 검색 결과가 많다면 요청이 있을 때마다 서버사이드에서 데이터 패칭하는 것이 부담될 것으로 생각됨
+const Search = () => {
+  const router = useRouter();
+  const q = router.query.q;
 
-  const result = await fetchSearchMovies(q as string);
+  const [result, setResult] = useState<MovieData[]>([]);
 
-  return {
-    props: { result },
+  const fetch = async () => {
+    const data = await fetchSearchMovies(q as string);
+    setResult(data);
   };
-};
 
-const Search = ({
-  result,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  useEffect(() => {
+    if (q) {
+      fetch();
+    }
+  }, [q]);
+
+  if (!result) return 'Loading...';
+
   return (
     <div className={style.container}>
       {result.map((item: MovieData) => (
